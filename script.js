ed1042598b7adb1277f9df48981e420f889a530f
Naveen Bandarage <thebanterage@gmail.com>
2021-01-20 16:57:23 +1300

    Created initial file to track and calculate the bus fare

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  readline.question('How much is on your ATHop? ', value => {
+    console.log(`The value is ${value}!`);
+    console.log("The amount left over after single trip is: ", subtractSingleTrip(value));
+    console.log("The amount after a day trip (back and forth is): ", subtractFullDay(value));
+    readline.close();
+  });
+  
+function subtractSingleTrip(a) { 
+    return num = parseFloat(a) - 1.55
+    } 
+
+function subtractFullDay(a) { 
+        return num = parseFloat(a) - 3
+        } 
+
+
